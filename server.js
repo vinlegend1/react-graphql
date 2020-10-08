6836c28f15a5a1ec62f992a7147197abd157f9e8
Vincent Hong <vinlegend1@gmail.com>
2020-10-08 10:24:14 +0800

    Created GraphQL server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+const schema = require('./schema');
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
